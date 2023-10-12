fe6c942f1844e8cf0586b9e414b2f7e617c95b27
Nico Coetzee <nicc777@gmail.com>
2023-10-12 06:36:19 +0200

    Added kubernetes information retrieval methods: get namespaces and pods in a namespace

--- src/kr8s_poc/main_server.py
+++ src/kr8s_poc/main_server.py
@@ -6,9 +6,6 @@ import sys
 
 
 logger = logging.getLogger(__name__)
-app = FastAPI()
-
-
 log_format = '%(asctime)s %(levelname)s - %(message)s'
 log_level = logging.INFO
 formatter = logging.Formatter(log_format)
@@ -16,6 +13,10 @@ h = logging.StreamHandler(sys.stdout)
 h.setLevel(log_level)
 h.setFormatter(formatter)
 logger.addHandler(h)
+
+
+app = FastAPI()
+logger.info('The App Is Ready')
 
 
 # This is also our liveness and readiness probe URI
@@ -29,6 +30,7 @@ async def namespaces():
     namespaces = list()
     for namespace in await kr8s.asyncio.get('namespaces'):
         logger.info('namespace={}'.format(namespace))
+        print('PRINT: namespace={}'.format(namespace))
         namespaces.append({"Name": namespace.metadata.name,})
     return {"Namespaces": namespaces}
 
@@ -38,6 +40,7 @@ async def namespaced_pods(namespace):
     pods = list()
     for pod in await kr8s.asyncio.get("pods", namespace=namespace):
         logger.info('pod={}'.format(pod))
+        print('PRINT: pod={}'.format(pod))
         pods.append(
             {
                 "Name": pod.metadata.name,
