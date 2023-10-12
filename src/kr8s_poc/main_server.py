036c2aab312b9913b9ef933138ce98fce0bcffb4
Nico Coetzee <nicc777@gmail.com>
2023-10-12 05:44:00 +0200

    Added kubernetes information retrieval methods: get namespaces and pods in a namespace

--- src/kr8s_poc/main_server.py
+++ src/kr8s_poc/main_server.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from kr8s.objects import Pod
+import kr8s
+
 
 app = FastAPI()
 
@@ -7,4 +10,23 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"status": "ok"}
+
+
+@app.get('/namespaces')
+async def namespaces():
+    namespaces = list()
+    for namespace in kr8s.asyncio.get('namespaces'):
+        namespaces.append(namespace)
+    return {"Namespaces": namespaces}
+
+
+@app.get('/namespace/{namespace}/pods')
+async def namespaced_pods(namespace):
+    pods = list()
+    for pod in await kr8s.asyncio.get("pods", namespace=namespace):
+        pods.append(pod.metadata.name)
+    return {
+        "Namespace": namespace,
+        "Pods": pods,
+    }
 
