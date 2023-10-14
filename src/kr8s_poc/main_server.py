9b944bc01fd2a726439cf704ef71fa241ffcf4f1
Nico Coetzee <nicc777@gmail.com>
2023-10-14 08:49:51 +0200

    Fixed namespace logic to add selected metadata

--- src/kr8s_poc/main_server.py
+++ src/kr8s_poc/main_server.py
@@ -6,6 +6,7 @@ import traceback
 import uvicorn
 from fastapi import FastAPI
 import kr8s
+import copy
 
 
 logger = logging.getLogger(__name__)
@@ -18,6 +19,12 @@ h.setFormatter(formatter)
 logger.addHandler(h)
 
 
+class KubeApi:
+    def __init__(self) -> None:
+        self.api = None
+
+
+kube_api = KubeApi()
 app = FastAPI()
 
 
@@ -31,12 +38,21 @@ async def root():
 async def namespaces():
     namespaces = list()
     result = dict()
+    logger.info('FUNCTION namespaces() CALLED')
     try:
-        for namespace in await kr8s.asyncio.get('namespaces'):
+        for namespace in await kr8s.asyncio.get('namespaces', api=kube_api.api):
+            raw_data = dict()
             logger.info('namespace={}   type={}'.format(namespace, type(namespace)))
             namespace_d = namespace.__dict__
-            logger.info('namespace_d={}   type={}'.format(json.dumps(namespace_d, default=str), type(namespace_d)))
-            namespaces.append({"Name": namespace.metadata.name, "RawObject": namespace_d,})
+            if '_raw' in namespace_d:
+                tmp_raw_data = copy.deepcopy(namespace_d['_raw'])
+                logger.info('raw_data={}   type={}'.format(json.dumps(tmp_raw_data, default=str), type(tmp_raw_data)))
+                if 'metadata' in tmp_raw_data:
+                    if 'creationTimestamp' in tmp_raw_data['metadata']:
+                        raw_data['CreateTimestamp'] = tmp_raw_data['metadata']['creationTimestamp']
+                    if 'labels' in tmp_raw_data['metadata']:
+                        raw_data['Labels'] = tmp_raw_data['metadata']['labels']
+            namespaces.append({"Name": namespace.metadata.name, "Metadata": raw_data,})
         result = {"Namespaces": namespaces}
         logger.info('result={}'.format(result))
         logger.info('returning result: {}'.format(json.dumps(result, default=str)))
@@ -49,6 +65,7 @@ async def namespaces():
 async def namespaced_pods(namespace):
     pods = list()
     result = dict()
+    logger.info('FUNCTION namespaced_pods() CALLED')
     try:
         for pod in await kr8s.asyncio.get("pods", namespace=namespace):
             logger.info('pod={}   type={}'.format(pod, type(pod)))
@@ -64,6 +81,9 @@ async def namespaced_pods(namespace):
     
 
 if __name__ == '__main__':
+    kube_config_file = os.getenv('KUBECONFIG', None)
+    if kube_config_file is not None:
+        kube_api.api = kr8s.api(kubeconfig=kube_config_file)
     current_file_dir = os.path.dirname(os.path.realpath(__file__))   
     src_part = '{}src{}'.format(os.sep, os.sep)
     if src_part in current_file_dir:
@@ -74,5 +94,6 @@ if __name__ == '__main__':
     else:
         print('Using DEFAULT LOG CONFIG')
         uvicorn.run(app, host="0.0.0.0", port=9080, workers=1)
+    print('DONE')
     
 
